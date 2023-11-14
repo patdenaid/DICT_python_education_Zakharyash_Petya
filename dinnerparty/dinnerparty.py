c1b8d8f103e3833f489ff80232ae67a0c35b9fe7
patdenaid <zakharyshpetya@gmail.com>
2023-11-15 00:37:18 +0200

    dinnerparty 1-st stage
    dinnerparty 2-nd stage
    
    dinnerparty 3-rd stage
    
    dinnerparty 4-th stage

---
+++ dinnerparty/dinnerparty.py
@@ -0,0 +1,44 @@
+import random
+
+def main():
+    # Етап 1: Зчитуємо кількість друзів
+    num_friends = int(input("Enter the number of friends joining (including you):\n"))
+
+    # Перевірка на коректність кількості людей
+    if num_friends <= 0:
+        print("No one is joining for the party")
+        return
+
+    # Етап 2: Зчитуємо імена друзів та зберігаємо їх у словнику
+    friends_dict = {}
+    print("Enter the name of every friend (including you), each on a new line:")
+    for _ in range(num_friends):
+        friend_name = input("> ")
+        friends_dict[friend_name] = 0
+
+    # Етап 3: Виводимо отриманий словник
+    print(friends_dict)
+
+    # Етап 4: Розділити рахунок порівну
+    total_amount = float(input("Enter the total amount:\n"))
+    if num_friends > 0:
+        per_person_amount = round(total_amount / num_friends, 2)
+
+        # Етап 5: Вибір "щасливчика"
+        lucky_choice = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No:\n")
+        if lucky_choice.lower() == 'yes':
+            lucky_one = random.choice(list(friends_dict.keys()))
+            print(f"{lucky_one} is the lucky one!")
+            # Обчислення суми для n-1 людей
+            for friend in friends_dict:
+                if friend != lucky_one:
+                    friends_dict[friend] = per_person_amount
+            friends_dict[lucky_one] = 0
+        else:
+            print("No one is going to be lucky")
+
+    # Етап 6: Виводимо оновлений словник
+    print(friends_dict)
+
+if __name__ == "__main__":
+    main()
