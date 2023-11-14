92fe4cebae6c3c262e4c417a10015c3faae8aa3b
patdenaid <zakharyshpetya@gmail.com>
2023-11-15 00:45:24 +0200

    dinnerparty 4-th stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -23,19 +23,22 @@ def main():
     total_amount = float(input("Enter the total amount:\n"))
     if num_friends > 0:
         per_person_amount = round(total_amount / num_friends, 2)
-        for friend in friends_dict:
-            friends_dict[friend] = per_person_amount
 
-    # Етап 5: Виводимо оновлений словник
+        # Етап 5: Вибір "щасливчика"
+        lucky_choice = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No:\n")
+        if lucky_choice.lower() == 'yes':
+            lucky_one = random.choice(list(friends_dict.keys()))
+            print(f"{lucky_one} is the lucky one!")
+            # Обчислення суми для n-1 людей
+            for friend in friends_dict:
+                if friend != lucky_one:
+                    friends_dict[friend] = per_person_amount
+            friends_dict[lucky_one] = 0
+        else:
+            print("No one is going to be lucky")
+
+    # Етап 6: Виводимо оновлений словник
     print(friends_dict)
-
-    # Етап 6: Вибір "щасливчика"
-    lucky_choice = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No:\n")
-    if lucky_choice.lower() == 'yes':
-        lucky_one = random.choice(list(friends_dict.keys()))
-        print(f"{lucky_one} is the lucky one!")
-    else:
-        print("No one is going to be lucky")
 
 if __name__ == "__main__":
     main()
